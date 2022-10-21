90d52c4892f4198779f3a04f80e9adf88735a7f1
Judy Yu <judy.yuqiuyi@gmail.com>
2022-10-21 15:31:31 -0400

    Calculate the time taken to sort an array

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -17,7 +17,7 @@ func waitForUserInput() {
 // create an empty list (array)
 var dataSet : [Int] = []
 
-// populate the list
+// populate the list: random array
 //for _ in 1...10 {
 //    dataSet.append(Int.random(in: 1...100))
 //}
@@ -37,7 +37,11 @@ dataSet.append(2)
 // print the list
 print("Unsorted:")
 print(dataSet)
-waitForUserInput()
+//waitForUserInput()
+
+// keep track of start time (epoch)
+// start time = number of seconds since Jan. 1st, 1970
+let startTime = Date().timeIntervalSince1970
 
 var m = 1
 // loop thought the entire array "n" times until no swaps in current pass
@@ -75,7 +79,7 @@ for i in 0..<dataSet.count - 1 {
     print("Array after pass \(i + 1):")
     print(dataSet)
     print("Swapping occured in this pass: \(swapped)")
-    waitForUserInput()
+    //waitForUserInput()
     
     // when no swap occurred, stop the outer loop
     if swapped == false {
@@ -84,3 +88,11 @@ for i in 0..<dataSet.count - 1 {
     }
     
 }
+
+// get end time (seconds since epoch)
+print("ended")
+let endTime = Date().timeIntervalSince1970
+
+// get time taken for array to sort
+let elaspsedTime = endTime - startTime
+print("Sort took \(elaspsedTime) seconds")
